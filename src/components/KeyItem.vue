323825f0f23869facb96c3686d4116d7299b52ef
mfsv <mfsv6697164@gmail.com>
2023-08-25 15:33:51 +0500

    first

---
+++ src/components/KeyItem.vue
@@ -0,0 +1,66 @@
+<template>
+    <div
+        :class="{'white-key': !keyName.includes('b'), 'black-key': keyName.includes('b')}"
+        class="key"
+    >
+        <span style="margin-bottom: 1rem;"> {{ keyName }} </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "KeyItem",
+    props: {
+        keyName: {
+            type: String,
+            required: true
+        },
+        note: {
+            type: String,
+            required: true
+        },
+        keyCode: {
+            type: String,
+            required: true
+        },
+        src: {
+            type: String,
+            required: true
+        }
+    },
+
+
+}
+</script>
+
+<style scoped>
+.key {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    font-size: .7rem;
+    font-family: sans-serif;
+    user-select: none;
+    cursor: pointer;
+}
+
+.key:active, .key.active {
+    background: lime;
+}
+.white-key {
+    width: 1.9rem;
+    height: 10rem;
+    background: #fff;
+
+}
+.black-key {
+    position: absolute;
+    background: #000;
+    height: 7rem;
+    color: white;
+    top: -1px;
+}
+.black-key:nth-child(1n) {
+    margin-left: -.8rem;
+}
+</style>
---
+++ src/components/PianoContainer.vue
@@ -0,0 +1,156 @@
+<template>
+    <table class="piano-container" >
+        <tr>
+            <td
+                v-for="key of keys"
+                :key="key.id"
+                :class="{'white-td': !key.name.includes('b'), 'black-td': key.name.includes('b')}"
+            >
+
+                <KeyItem
+                    :key-name="key.name"
+                    :note="key.name"
+                    :class="key.keyCode"
+                    :key-code="key.keyCode"
+                    :src="key.src"
+                    @mousedown="play(key.src)"
+                    ref="keyitem"
+                />
+            </td>
+        </tr>
+    </table>
+</template>
+
+<script>
+import KeyItem from "@/components/KeyItem";
+export default {
+    name: "PianoContainer",
+    components: {
+        KeyItem
+    },
+    data() {
+        return {
+            keyboard: {
+                a: 'C4',
+
+            },
+            keys: [
+                {id: 6, name: 'A0', src: 'A0.mp3', keyCode: ''},
+                {id: 7, name: 'Bb0', src: 'Bb0.mp3', keyCode: ''},
+                {id: 7, name: 'B0', src: 'B0.mp3', keyCode: ''},
+                {id: 8, name: 'C1', src: 'C1.mp3', keyCode: ''},
+                {id: 9, name: 'Db1', src: 'Db1.mp3', keyCode: ''},
+                {id: 9, name: 'D1', src: 'D1.mp3', keyCode: ''},
+                {id: 10, name: 'Eb1', src: 'Eb1.mp3', keyCode: ''},
+                {id: 10, name: 'E1', src: 'E1.mp3', keyCode: ''},
+                {id: 11, name: 'F1', src: 'F1.mp3', keyCode: ''},
+                {id: 12, name: 'Gb1', src: 'Gb1.mp3', keyCode: ''},
+                {id: 12, name: 'G1', src: 'G1.mp3', keyCode: ''},
+                {id: 13, name: 'Ab1', src: 'Ab1.mp3', keyCode: ''},
+                {id: 13, name: 'A1', src: 'A1.mp3', keyCode: ''},
+                {id: 14, name: 'Bb1', src: 'Bb1.mp3', keyCode: ''},
+                {id: 14, name: 'B1', src: 'B1.mp3', keyCode: ''},
+                {id: 15, name: 'C2', src: 'C2.mp3', keyCode: 'z'},
+                {id: 16, name: 'Db2', src: 'Db2.mp3', keyCode: 's'},
+                {id: 16, name: 'D2', src: 'D2.mp3', keyCode: 'x'},
+                {id: 17, name: 'Eb2', src: 'Eb2.mp3', keyCode: 'd'},
+                {id: 17, name: 'E2', src: 'E2.mp3', keyCode: 'c'},
+                {id: 18, name: 'F2', src: 'F2.mp3', keyCode: 'v'},
+                {id: 19, name: 'Gb2', src: 'Gb2.mp3', keyCode: 'g'},
+                {id: 19, name: 'G2', src: 'G2.mp3', keyCode: 'b'},
+                {id: 20, name: 'Ab2', src: 'Ab2.mp3', keyCode: 'h'},
+                {id: 20, name: 'A2', src: 'A2.mp3', keyCode: 'n'},
+                {id: 21, name: 'Bb2', src: 'Bb2.mp3', keyCode: 'j'},
+                {id: 21, name: 'B2', src: 'B2.mp3', keyCode: 'm'},
+                {id: 22, name: 'C3', src: 'C3.mp3', keyCode: ','},
+                {id: 23, name: 'Db3', src: 'Db3.mp3', keyCode: 'l'},
+                {id: 23, name: 'D3', src: 'D3.mp3', keyCode: '.'},
+                {id: 24, name: 'Eb3', src: 'Eb3.mp3', keyCode: ';'},
+                {id: 24, name: 'E3', src: 'E3.mp3', keyCode: '/'},
+                {id: 25, name: 'F3', src: 'F3.mp3', keyCode: 'Shift'},
+                {id: 26, name: 'Gb3', src: 'Gb3.mp3', keyCode: 'Enter'},
+                {id: 26, name: 'G3', src: 'G3.mp3', keyCode: ''},
+                {id: 27, name: 'Ab3', src: 'Ab3.mp3', keyCode: ''},
+                {id: 27, name: 'A3', src: 'A3.mp3', keyCode: ''},
+                {id: 28, name: 'Bb3', src: 'Bb3.mp3', keyCode: ''},
+                {id: 28, name: 'B3', src: 'B3.mp3', keyCode: ''},
+                {id: 29, name: 'C4', src: 'C4.mp3', keyCode: 'q'},
+                {id: 30, name: 'Db4', src: 'Db4.mp3', keyCode: '2'},
+                {id: 30, name: 'D4', src: 'D4.mp3', keyCode: 'w'},
+                {id: 31, name: 'Eb4', src: 'Eb4.mp3', keyCode: '3'},
+                {id: 31, name: 'E4', src: 'E4.mp3', keyCode: 'e'},
+                {id: 32, name: 'F4', src: 'F4.mp3', keyCode: 'r'},
+                {id: 33, name: 'Gb4', src: 'Gb4.mp3', keyCode: '5'},
+                {id: 33, name: 'G4', src: 'G4.mp3', keyCode: 't'},
+                {id: 34, name: 'Ab4', src: 'Ab4.mp3', keyCode: '6'},
+                {id: 34, name: 'A4', src: 'A4.mp3', keyCode: 'y'},
+                {id: 35, name: 'Bb4', src: 'Bb4.mp3', keyCode: '7'},
+                {id: 35, name: 'B4', src: 'B4.mp3', keyCode: 'u'},
+                {id: 36, name: 'C5', src: 'C5.mp3', keyCode: 'i'},
+                {id: 37, name: 'Db5', src: 'Db5.mp3', keyCode: '9'},
+                {id: 37, name: 'D5', src: 'D5.mp3', keyCode: 'o'},
+                {id: 38, name: 'Eb5', src: 'Eb5.mp3', keyCode: '0'},
+                {id: 38, name: 'E5', src: 'E5.mp3', keyCode: 'p'},
+                {id: 39, name: 'F5', src: 'F5.mp3', keyCode: "["},
+                {id: 40, name: 'Gb5', src: 'Gb5.mp3', keyCode: '='},
+                {id: 40, name: 'G5', src: 'G5.mp3', keyCode: ']'},
+                {id: 41, name: 'Ab5', src: 'Ab5.mp3', keyCode: 'Backspace'},
+                {id: 41, name: 'A5', src: 'A5.mp3', keyCode: '\\'},
+                {id: 42, name: 'Bb5', src: 'Bb5.mp3', keyCode: 'Insert'},
+                {id: 42, name: 'B5', src: 'B5.mp3', keyCode: 'Delete'},
+            ]
+        }
+    },
+    computed: {
+      whiteKey() {
+          return  this.keys.filter(item => !item.name.includes('b'))
+      },
+      blackKey() {
+          return  this.keys.filter(item => item.name.includes('b'))
+      }
+    },
+    methods: {
+        play(src){
+            let note = new Audio(require('@/assets/notes/' + src));
+            note.play()
+        }
+    },
+    mounted() {
+        window.addEventListener('keydown', e => {
+            console.log(e.key)
+            this.$refs.keyitem.forEach(item => {
+                if(item.keyCode === e.key) {
+                    this.play(item.src)
+                    item.$el.classList.add('active')
+                }
+            })
+        })
+        window.addEventListener('keyup', e => {
+            this.$refs.keyitem.forEach(item => {
+                if(item.keyCode === e.key) {
+                    item.$el.classList.remove('active')
+                }
+            })
+        })
+    }
+
+}
+</script>
+
+<style scoped>
+.piano-container {
+    margin: 0 auto;
+    justify-content: center;
+    align-items: center;
+    border-collapse: collapse;
+    position: relative;
+    overflow: hidden;
+}
+td {
+    border: 1px solid #000;
+}
+.black-td {
+    border: none;
+    width: 0;
+}
+</style>
